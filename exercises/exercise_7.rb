66b2374889b11f981cdc272fc1e6a18e5d9f33a4
jeff-sexton <sexton.jeffp@gmail.com>
2020-04-27 20:46:07 -0700

    Added validation and user input

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,32 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name: "
+name = gets.chomp
+
+print "What is your annual revenue: "
+revenue = gets.chomp.to_i
+
+print "Do you sell mens apparel? (y/n): "
+ans = gets.chomp
+  if ans == 'y'
+    mens = true
+  else
+    mens = false
+  end
+
+print "Do you sell womens apparel? (y/n): "
+ans = gets.chomp
+  if ans == 'y'
+    womens = true
+  else
+    womens = false
+  end
+
+newStore = Store.create(name: name, annual_revenue: revenue, mens_apparel: mens, womens_apparel: womens)
+
+if newStore.valid?
+  puts newStore.inspect
+else
+  newStore.errors.messages.each { |key, value| puts "#{key}: #{value[0]}"}
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,20 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {
+    only_interger: true,
+    greater_than_or_equal_to: 0,
+  }
+  validate :must_sell_something
+
+
+
+  def must_sell_something
+    if !self.mens_apparel && !self.womens_apparel
+      errors.add(:mens_apparel, "must sell at least one type of apparel")
+      # errors.add(:womens_apparel,"must sell at least one type of apparel")
+    end
+  end
+
 end
