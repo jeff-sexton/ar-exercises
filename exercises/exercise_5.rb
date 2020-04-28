e97eb0c6b90cd801bab575fb81e64791dca73ac1
jeff-sexton <sexton.jeffp@gmail.com>
2020-04-27 19:07:32 -0700

    Added calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total revenue: $#{Store.sum('annual_revenue')}"
+
+puts "Average anual revenue: $#{Store.average('annual_revenue')}"
+
+puts "Stores with $1,000,000 in anual sales: #{Store.where("annual_revenue > 1000000").count()}"
